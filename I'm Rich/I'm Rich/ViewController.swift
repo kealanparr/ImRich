29eff67f857978ea81f08d657f115c1e908ca630
AnonRobot <kea_parr@hotmail.co.uk>
2021-03-18 17:28:44 +0000

    Add files via upload

---
+++ I'm Rich/I'm Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Kealan Parr on 25/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
